34f73a050db5613f39e4c40ba3e041f7f70ecbab
Gkinalas, Ioannis <ioannis.gkinalas@astrazeneca.com>
2017-10-25 17:17:02 +0100

    RAUK-1818: Initial Commit

---
+++ AutomatedDMTA-model/src/main/java/com/astrazeneca/rd/AutomatedDMTA/model/Compound.java
@@ -0,0 +1,74 @@
+package com.astrazeneca.rd.AutomatedDMTA.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Andy Gibson
+ * 
+ * Simple model class that is used to test JPA
+ *
+ */
+@Entity
+@Table
+public class Compound {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(length = 24)
+	@Size(min = 1, max = 24)
+	@NotNull
+	private String firstName;
+
+	@Column(length = 24)
+	@Size(min = 1, max = 24)
+	@NotNull
+	private String lastName;
+
+	public Compound() {}
+	
+	public Compound(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("Person [");
+		sb.append("ID : ").append(id).append(", First Name : ").append(firstName).append(", Last Name : ").append(lastName).
+			append("]");
+		return sb.toString();
+	}
+}
